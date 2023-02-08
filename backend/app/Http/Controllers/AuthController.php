913b67cb6f9ea07fd7deaadfa8c087dd68ddf143
Kzulfazriawan <kzulfazriawan@gmail.com>
2023-02-08 16:07:06 +0700

    Update backend and frontend

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails())
+            return response(['errors'=>$validator->errors()->all()], 422);
+
+        $user = User::where("username", $request->username)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('BSIT2023' . $user->username)->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" =>'User does not exist'];
+            return response($response, 422);
+        }
+    }
 }
---
+++ backend/database/migrations/2023_02_08_054605_create_ticket_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->enum('status', ['new', 'progress', 'closed']);
+            $table->foreignId('report_id');
+            $table->foreignId('admin_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropForeign(['report_id', 'admin_id']);
+            $table->dropColumn('report_id', 'admin_id');
+        });        
+        Schema::dropIfExists('tickets');
+    }
+};
